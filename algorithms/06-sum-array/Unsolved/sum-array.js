2f436dcc4297514e50b427bf78d90c6ae9e741e5
Staroyee <danmasefield@gmail.com>
2023-12-08 21:29:31 +1030

    Completed Algorithm 6

--- algorithms/06-sum-array/Unsolved/sum-array.js
+++ algorithms/06-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,7 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function(arr) {
+    return arr.reduce(function(a, b) {
+        return a + b;
+    })
+};
