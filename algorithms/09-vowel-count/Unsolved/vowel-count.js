85ea4ecf743dcd203a95761fccde1e2a118ea211
Staroyee <danmasefield@gmail.com>
2023-12-10 19:17:40 +1030

    Completed Algorithm 9

--- algorithms/09-vowel-count/Unsolved/vowel-count.js
+++ algorithms/09-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,18 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function (str) {
+  // set variable to keep track of vowel counts
+  let vowelsCount = 0;
+  // create an array of vowels using capitals and lowercase
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  // Loop through the provided string
+  for (let char of str) {
+    // Compare each char in the string to the chars in the vowels array
+    if (vowels.includes(char)) {
+      // If the char in the string matches the char in the array it will add 1 to the vowelsCount variable
+      vowelsCount++;
+    }
+  }
+  // Return the value of the vowels count once the string has finished iterating through.
+  return vowelsCount;
+};
