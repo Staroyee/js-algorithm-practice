9d6d81771bca28fdb97ddd7414ccb1f42dadb9a3
Staroyee <danmasefield@gmail.com>
2023-12-24 10:55:13 +1030

    Completed title-case Algorithm

--- algorithms/11-title-case/Unsolved/title-case.js
+++ algorithms/11-title-case/Unsolved/title-case.js
@@ -1,3 +1,10 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    return str.replace(
+        /([^\W_]+[^\s-]*) */g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    )
+};
