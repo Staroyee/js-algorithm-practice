b631fad7a2c9658e131a6526d3ad4a5389d5d4cb
Staroyee <danmasefield@gmail.com>
2023-12-27 22:58:37 +1030

    Completed Character-count algorithm

--- algorithms/13-character-count/Unsolved/character-count.js
+++ algorithms/13-character-count/Unsolved/character-count.js
@@ -1,3 +1,17 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function (str) {
+  const charCount = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    // console.log(char);
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+  console.log(charCount);
+  return charCount;
+};
