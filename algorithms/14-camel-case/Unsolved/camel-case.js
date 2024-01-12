e4862ba49fd07c4ad4c14dfbb988930145fa1fe8
Staroyee <danmasefield@gmail.com>
2024-01-12 21:21:34 +1030

    Completed camel-case algorithm

--- algorithms/14-camel-case/Unsolved/camel-case.js
+++ algorithms/14-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,8 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function (str) {
+  let camelChar = str.toLowerCase();
+  return camelChar
+    .split(" ")
+    .reduce((s, c) => s + (c.charAt(0).toUpperCase() + c.slice(1)));
+};
