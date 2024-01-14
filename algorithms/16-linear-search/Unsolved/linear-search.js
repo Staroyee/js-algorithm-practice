7b33f1859a30400b13d0c31af3b3eced3ba0efb0
Staroyee <danmasefield@gmail.com>
2024-01-14 21:49:29 +1030

    Completed linear-search algorithm

--- algorithms/16-linear-search/Unsolved/linear-search.js
+++ algorithms/16-linear-search/Unsolved/linear-search.js
@@ -3,4 +3,16 @@
 // else return -1
 // You may not use the `indexOf`, `lastIndexOf` or `includes` methods
 
-var linearSearch = function(arr, target) {};
+var linearSearch = function (arr, target) {
+  // loop over the array of numbers
+  for (i = 0; i < arr.length; i++) {
+    // check if the number at each index is equal to the target number
+    if (arr[i] === target) {
+      console.log(arr[i]);
+      // return the index of the target number if it is in the array
+      return i;
+    }
+  }
+  // if the number is not in the array return -1
+  return -1;
+};
