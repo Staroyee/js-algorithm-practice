6b521fef65a4deb1f3a68593f2be3ba850681b38
Staroyee <danmasefield@gmail.com>
2024-01-18 20:44:07 +1030

    Completed is-anagram algorithm

--- algorithms/17-is-anagram/Unsolved/is-anagram.js
+++ algorithms/17-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,15 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+    strA = strA.replace(/\s/g, '').toLowerCase();
+    strB = strB.replace(/\s/g, '').toLowerCase();
+
+    if (strA.length !== strB.length) {
+        return false;
+    }
+    strA = strA.split('').sort().join('');
+    strB = strB.split('').sort().join('');
+
+    return strA === strB
+};
